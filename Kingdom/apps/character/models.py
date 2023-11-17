2aa579976200cb39e55ff0e346c4c19ba4af8eee
Telpegaraf <telpegaraf@gmail.com>
2023-11-17 19:12:02 +0400

    added model God

--- Kingdom/apps/character/models.py
+++ Kingdom/apps/character/models.py
@@ -1,1 +1,5 @@
 from django.db import models
+
+
+class MoralIntentions(models.Model):
+    name = models.CharField(max_length=100)
--- Kingdom/apps/god/models.py
+++ Kingdom/apps/god/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from apps.character.models import MoralIntentions
 
-# Create your models here.
+
+class Domains(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class God(models.Model):
+    name = models.CharField(max_length=50)
+    edict = models.CharField(max_length=100)
+    anathema = models.CharField(max_length=100)
+    areas_of_interest = models.CharField(max_length=100)
+    temples = models.CharField(max_length=100)
+    worship = models.CharField(max_length=100)
+    sacred_animal = models.CharField(max_length=100)
+    sacred_color = models.CharField(max_length=100)
+    domain = models.ForeignKey(Domains, on_delete=models.CASCADE, related_name='domain')
+    chosen_weapon = models.CharField(max_length=100)
+    moral_intentions = models.ManyToManyField(MoralIntentions, on_delete=models.CASCADE,
+                                              related_name='moral_intentions')
