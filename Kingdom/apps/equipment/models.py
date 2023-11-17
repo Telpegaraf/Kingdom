e20fe877ce27d0358ffe66b3b526226f1ebc72d9
Telpegaraf <telpegaraf@gmail.com>
2023-11-17 18:54:29 +0400

    added Armor models

--- Kingdom/apps/equipment/models.py
+++ Kingdom/apps/equipment/models.py
@@ -1,3 +1,53 @@
 from django.db import models
 
-# Create your models here.
+
+class Armor(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
+
+    class Meta:
+        abstract = True
+
+
+class Helmet(Armor):
+    pass
+
+
+class PlateArmor(Armor):
+    pass
+
+
+class FirstWeapon(Armor):
+    pass
+
+
+class SecondWeapon(Armor):
+    pass
+
+
+class FirstRing(Armor):
+    pass
+
+
+class SecondRing(Armor):
+    pass
+
+
+class Amulet(Armor):
+    pass
+
+
+class Gloves(Armor):
+    pass
+
+
+class FirstBracer(Armor):
+    pass
+
+
+class SecondBracer(Armor):
+    pass
+
+
+class Boots(Armor):
+    pass
