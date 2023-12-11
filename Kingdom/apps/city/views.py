7f7f092bf1532c28360ffa76c34d16015fa6565d
Telpegaraf <telpegaraf@gmail.com>
2023-12-11 12:22:23 +0400

    added CityOverallView

--- Kingdom/apps/city/views.py
+++ Kingdom/apps/city/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from .serializers import CityOverallSerializer
+from .models import Kingdom
 
-# Create your views here.
+
+class CityOverall(APIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = CityOverallSerializer
+
+    def get(self, request, *args, **kwargs):
+        kingdom = Kingdom.objects.all()
+        serializer = self.serializer_class(kingdom, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
