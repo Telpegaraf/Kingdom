33a6a3026d5d42441dddac573055aae12ce62e9b
Telpegaraf <telpegaraf@gmail.com>
2023-12-12 18:52:56 +0400

    added equipview

--- Kingdom/apps/equipment/views.py
+++ Kingdom/apps/equipment/views.py
@@ -2,15 +2,15 @@ from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
-#from .serializers import EquipmentOverallSerializer
+from .serializers import EquipmentOverallSerializer
 from .models import Equipment
 
 
-# class EquipmentOverall(APIView):
-#     permission_classes = [IsAuthenticated]
-#     serializer_class = EquipmentOverallSerializer
-#
-#     def get(self, request, *args, **kwargs):
-#         equipment = Equipment.objects.all()
-#         serializer = self.serializer_class(equipment, many=True)
-#         return Response(serializer.data, status=status.HTTP_200_OK)
+class EquipmentOverall(APIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = EquipmentOverallSerializer
+
+    def get(self, request, *args, **kwargs):
+        equipment = Equipment.objects.all()
+        serializer = self.serializer_class(equipment, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
